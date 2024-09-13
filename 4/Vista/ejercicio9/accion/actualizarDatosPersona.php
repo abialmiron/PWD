19217ebcfbc1ac7858807f45ac72882075082d16
Dante Avila <danteavila1@hotmail.com>
2024-09-12 22:33:44 -0300

    agregado mensaje al mostrar resultado

--- 4/Vista/ejercicio9/accion/actualizarDatosPersona.php
+++ 4/Vista/ejercicio9/accion/actualizarDatosPersona.php
@@ -9,12 +9,29 @@ $datos = data_submitted();
 
 // Intenta modificar los datos en la base de datos
 if ($abmPersona->modificacion($datos)) {
-        echo "Los datos fueron actualizados correctamente.";
+        $mensaje = "Los datos fueron actualizados correctamente.";
     } else {
-        echo "Error al actualizar los datos.";
+        $mensaje = "Error al actualizar los datos.";
     }
 
 ?>
+
+<main class="index">
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title text-center"><?php echo $mensaje;?></h5>
+                        <div class="text-center p-2">
+                            <a class="btn btn-secondary" href="../buscarPersona.php">Volver</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php
     include('../../Templates/footer.php');
